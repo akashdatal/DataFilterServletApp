b6bbec6bb32753e33de6c64977f71f75d47942e0
akashdatal <emailid>
2025-06-25 18:56:28 +0530

    Initial commit

--- DataFilteringBasedOnDataRange/src/main/java/com/nt/servlet/FetchFilteredDataServlet.java
+++ DataFilteringBasedOnDataRange/src/main/java/com/nt/servlet/FetchFilteredDataServlet.java
@@ -29,22 +29,12 @@ public class FetchFilteredDataServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("text/html");
 		PrintWriter out= res.getWriter();
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>Filtered Posts</title>");
+		
 		out.println("<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css\' rel=\'stylesheet\' integrity=\'sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT\' crossorigin=\'anonymous'>");
-		out.println("</head>");
-		out.println("<body class='bg-light'>");
 
 		out.println("<div class='container mt-5'>");
 		String startDateStr = req.getParameter("startDate");
 		String endDateStr = req.getParameter("endDate");
-		
-		if(startDateStr == null || endDateStr == null) {
-			out.println("<h3>Error: Missing date parameters.</h3>");
-			return;
-		}
 		
 		try {
 			Date startDate = sdf.parse(startDateStr);
@@ -74,8 +64,7 @@ public class FetchFilteredDataServlet extends HttpServlet {
 				out.println("<td>"+post.getTitle()+"</td>");
 				out.println("<td>"+post.getDescription()+"</td>");
 				out.println("<td>"+post.getDate()+"</td>");
-				out.println("</tr>");	
-				
+				out.println("</tr>");
 			}
 			out.println("</table>");
 			out.println("<a href='index.html' class='btn btn-primary mt-3'>Home</a>");
@@ -83,14 +72,13 @@ public class FetchFilteredDataServlet extends HttpServlet {
 			}
 			out.println("</div>");
 
-			out.println("</body>");
-			out.println("</html>");
 		}catch (Exception e) {
 			out.println("<h2>Error processing request: " + e.getMessage() + "</h3>");
 			e.printStackTrace();
 		}
 	}
 	private List<PostObject> fetchPostObjectFromAPI() throws IOException{
+		
 		URL url = new URL(API_URL);
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
@@ -103,7 +91,6 @@ public class FetchFilteredDataServlet extends HttpServlet {
 		for(PostObject post :posts) {
 			post.setId(post.getId());
 		}
-		
 		return posts;
 	}
 }
